aefc4920d3eeac794e1e0779d454a613daad0a09
Heather Frantisak <hfrantisak@mvnp.com>
2017-05-26 09:27:32 -1000

    Refactor ListView into separate list component

--- App.js
+++ App.js
@@ -1,47 +1,15 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, ListView, Text, View } from 'react-native';
 
-import Row from './components/Row';
-
-const jsonUrl = 'https://data.sfgov.org/resource/bm46-8iwk.json';
+import ArtList from './components/ArtList';
 
 export default class App extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    
-    this.state = {
-      dataSource: ds.cloneWithRows({})
-    };
-  }
-  
-  componentWillMount() {
-    fetch(jsonUrl)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData)
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .done();
-  }
-    
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.heading}>Navigation</Text>
-        <Text style={styles.heading}>SF Civic Art Collection</Text>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Row {...rowData} />}
-        />
+        <ArtList />
       </View>
     );
   }
---
+++ components/ArtList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, ListView, Text, View } from 'react-native';
+
+import Row from './Row';
+
+const jsonUrl = 'https://data.sfgov.org/resource/bm46-8iwk.json';
+
+export default class ArtList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.state = {
+      dataSource: ds.cloneWithRows({})
+    };
+  }
+
+  componentWillMount() {
+    fetch(jsonUrl)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData)
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .done();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>SF Civic Art Collection</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Row {...rowData} />}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 30,
+  },
+});
