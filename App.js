2cce5f1b6c74153bcbe5331dadbed762cf78af73
Heather Frantisak <hfrantisak@mvnp.com>
2017-05-26 14:56:02 -1000

    Replace home component with StackNavigation, setting home to be initialRoute

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationProvider router={Router}>
-        <HomeScreen />
+        <StackNavigation initialRoute='home' />
       </NavigationProvider>
     );
   }
